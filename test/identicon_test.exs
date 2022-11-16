4135596f8af1377f370a011d627f4594485ab54d
JamesG <57632668+JimmyMcKegger@users.noreply.github.com>
2022-11-16 10:43:34 +0000

    Update identicon_test.exs

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,7 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "Makes an identicon" do
+    assert Identicon.make("test") == :ok
   end
 end
